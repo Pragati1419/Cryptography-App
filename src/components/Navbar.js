e384974e9f23c5d7c1d4c59e1d56d564e3bbcc33
Pragati Kumari <pragatikri31@gmail.com>
2023-07-29 21:12:23 +0530

    Cryptography

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+import React from "react";
+import { FaCoins } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <Link to='/'>
+            <div className='navbar'>
+                <FaCoins className='icon' />
+                <h1> Coin <span className='purple'>Search</span></h1>
+            </div>
+        </Link>
+    )
+}
+
+export default Navbar
